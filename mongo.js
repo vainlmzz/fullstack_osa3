ab55cb7530ceb28176d42c355926381edfae40ab
vainlmzz <vainlmzz@student.jyu.fi>
2021-08-13 13:36:28 +0300

    3.12

---
+++ mongo.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+
+
+const password = process.argv[2]
+const newName = process.argv[3]
+const newNumber = process.argv[4]
+
+const url =
+`mongodb+srv://fullstack:${password}@cluster0.4dbqx.mongodb.net/phonebook?retryWrites=true&w=majority`
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+
+
+const personSchema = new mongoose.Schema({ 
+  id: Number,
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3 ) {
+    console.log('Phonbook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+        console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+
+}
+
+
+if (process.argv.length === 5 ) {
+
+    
+    const person = new Person(
+        {
+        name:  newName,
+        number: newNumber
+        
+        }
+    )
+    
+    person.save().then(response => {
+      console.log(`added ${person.name} number ${person.number} to phonebook`)
+      mongoose.connection.close()
+    })
+    
+}
+
