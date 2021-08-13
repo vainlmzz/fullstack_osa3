8fb3b26855fa176ef25009ffdf59bff865eaef1e
vainlmzz <vainlmzz@student.jyu.fi>
2021-08-13 21:33:51 +0300

    3.15-3.18*

--- index.js
+++ index.js
@@ -13,23 +13,62 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
+  next(error)
+}
+
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+  .then(persons => {
     response.json(persons)
   })
+  .catch(error => next(error))
 })
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
+})
+
+app.get('/info', (req, res) => {
+  Person.countDocuments({}).then(lkm =>{
+    res.send(`<div><p>Phonebook has info for ${lkm} people</p><p>${date = new Date()}</p></div>`)
+  })
+  .catch(error => next(error))
+})
+
 
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
+  const n = body.name
+  const exists = Person.find({n:{$exists:true}})
+  
 
   if (!body.name || !body.number) {
     return response.status(400).json({ 
     error: 'add missing name/number' 
+    })  
+  }
+
+  if (exists) {
+    return response.status(409).json({ 
+        error: 'name must be unique' 
     })
-}
+    }
+
 
   const person = new Person({
     name: body.name,
@@ -40,6 +79,34 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
@@ -47,104 +114,4 @@ const PORT = process.env.PORT
     console.log(`Server running on port ${PORT}`)
 })
        
- /* 
-
-
- app.post('/api/persons', (request, response) => {
-  const body = request.body
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({ 
-    error: 'add missing name/number' 
-    })
-}
-
-  const person = new Person({
-    name: body.name,
-    number: body.number
-  })
-
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
-})
-
-
-app.get('/info', (req, res) => {
-    res.send(`<div><p>Phonebook has info for ${persons.length} people</p><p>${date = new Date()}</p></div>`)
-  })
-
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
-})
-
-
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Pekka Kuusi",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122"
-  }
-]
-
-
-
- app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
-})
-
  
-
-app.post('/api/persons', (request, response) => {
-const body = request.body
-
-
-if (!body.name || !body.number) {
-    return response.status(400).json({ 
-    error: 'add missing name/number' 
-    })
-}
-
-
-if (persons.map(person => person.name).includes(body.name)) {
-    return response.status(409).json({ 
-        error: 'name must be unique' 
-    })
-    }
-
-
-
-const person = {
-    id: Math.floor(Math.random() * 100),
-    name: body.name,
-    number: body.number
-}
-
-persons = persons.concat(person)
-
-response.json(person)
-})
-
-*/
-  
