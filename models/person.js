22f968c05e5c30f56d78fd5d0e34a9fe23834cfc
vainlmzz <vainlmzz@student.jyu.fi>
2021-08-15 23:40:33 +0300

    3.19-3.20*

--- models/person.js
+++ models/person.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -14,10 +14,12 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   })
 
 const personSchema = new mongoose.Schema({
-id: Number,
-name: String,
-number: String,
+  id: Number,
+  name: {type: String, unique: true, minlength: 3},
+  number: {type: String, minlength: 8}
 })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
